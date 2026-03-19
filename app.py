992b97d61b0115a11e1120df00e3ed3eb2781a82
Namitha1205 <koyinamitha1205.sse@saveetha.com>
2026-03-19 12:49:22 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, request, jsonify
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+
+app = Flask(__name__)
+
+url = "https://storage.googleapis.com/download.tensorflow.org/data/creditcard.csv"
+data = pd.read_csv(url)
+
+# Prepare data
+X = data.drop('Class', axis=1)
+y = data['Class']
+
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+# Train model
+model = LogisticRegression(max_iter=2000, class_weight='balanced')
+model.fit(X_scaled, y)
+
+@app.route('/')
+def home():
+    return "Fraud Detection API is running"
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    input_data = request.json['data']
+    
+    input_scaled = scaler.transform([input_data])
+    prob = model.predict_proba(input_scaled)[0][1]
+
+    if prob > 0.8:
+        risk = "HIGH RISK"
+    elif prob > 0.5:
+        risk = "MEDIUM RISK"
+    else:
+        risk = "LOW RISK"
+
+    return jsonify({
+        "risk_score": float(prob),
+        "status": risk
+    })
+
+import os
+
+port = int(os.environ.get("PORT", 10000))
+app.run(host='0.0.0.0', port=port)
